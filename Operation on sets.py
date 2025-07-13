4e3978568abbff1d3c8712d5e6807b5c97fa1f0f
Darkboy236 <aymaanafeez@gmail.com>
2025-07-13 18:37:09 +0100

    Projects

---
+++ Operation on sets.py
@@ -0,0 +1,23 @@
+# Different types of sets in python
+# set of intergers
+my_set = {1, 2, 3, 4, 5}
+
+# set of mixed datatypes
+my_set = {1, "Hello", (1, 2, 3)}
+print(my_set)
+
+# set cannot have duplicates
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+
+# we can make set from a list
+my_set = set([1, 2, 3, 2])
+print(my_set,"\n")
+
+# remove a number from a set
+num_set = {0, 1, 3, 4, 5}
+print("Original set:")
+print(num_set)
+num_set.pop()
+print("After removing the first element from the said set:")
+print(num_set,"\n")
